8beb282dca0a761cf121e86a1aa27c8278a33ba8
Strath <strath@donat.org>
2018-03-12 22:06:05 -0500

    v0.0.7

---
+++ settings.lua
@@ -0,0 +1,15 @@
+data:extend({
+	{
+		type = "int-setting",
+		name = "DeleteEmptyChunks_radius",
+		setting_type = "runtime-global",
+		minimum_value = 0,
+		default_value = 2
+	},
+	{
+		type = "bool-setting",
+		name = "DeleteEmptyChunks_concrete",
+		setting_type = "runtime-global",
+		default_value = false
+	}
+})
